5b23246df7db8582fa19f7906d66e2048c6f32ab
alikarimi <karimi.me.ali@gmail.com>
2022-11-26 09:02:58 +0330

    simple user page

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({  // we have access to Vue from cdn
     data() {
         return {
-            firstName: 'Ali'
+            firstName: 'Ali',
+            lastName: 'Karimi',
+            email: 'karimi.me.ali@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
     }
 })
